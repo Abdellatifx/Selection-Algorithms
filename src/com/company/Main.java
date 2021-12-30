4b5d326f35d591bea5c5e90c70d507f28a2a681d
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2021-12-30 16:17:15 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
+
 import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	MedianCalculator obj1 = new MedianCalculator();
- //   obj1.calculateMedian();
+        MedianCalculator obj1 = new divide();
+        int[] array = {1, 2, 3, 4};
+
+        obj1.calculateMedian(array, 0, array.length);
     }
 }
