0e2ec206e90104d4af0df53c93309988d9c7b327
Abdellatifx <58610681+Abdellatifx@users.noreply.github.com>
2021-12-30 14:15:41 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MedianCalculator obj1 = new MedianCalculator();
+ //   obj1.calculateMedian();
+    }
+}
